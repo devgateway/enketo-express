049361146e1b37de4ab960fcc408c0c1eb855fb8
dbianco <dbianco@developmentgateway.org>
2020-08-10 16:22:29 -0300

    Fix to add locales in to the build

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
 GENERATE_SOURCEMAP=false
 SRC="./public"
 TARGET="/opt/enketo-express/public"
+SRC2="./locales"
+TARGET2="/opt/enketo-express/locales"
 
 if [ -d $SRC ]; then
   rm -Rf /opt/enketo-express/public
+  rm -Rf /opt/enketo-express/locales
   cp -R $SRC $TARGET
+  cp -R $SRC2 $TARGET2
   sudo systemctl restart enketo
 else
   echo "Dir not found: $SRC"
